8075afd35ab29e5c3f125a93d00946f0a9972891
Pourtier Jacques <jacques.pourtier@etu.u-bordeaux.fr>
2020-03-31 18:34:02 +0200

    tiled sample of damier 3

--- easypap-se/kernel/c/sable.c
+++ easypap-se/kernel/c/sable.c
@@ -130,56 +130,8 @@ unsigned sable_compute_seq (unsigned nb_iter)
 
 // /////////////////////////////// Version séquentielle simple mais optimisé (seq_opt)
 
-static inline void compute_new_state_tiled (int y, int x)
-{
-  if (table (y, x) >= 4) {
-    unsigned long int div4 = table (y, x) / 4;
-    table (y, x - 1) += div4;
-    table (y, x + 1) += div4;
-    table (y - 1, x) += div4;
-    table (y + 1, x) += div4;
-    table (y, x) %= 4;
-    changement = 1;
-  }
-}
 
-static void do_tile_tiled (int x, int y, int width, int height, int who)
-{
-  PRINT_DEBUG ('c', "tuile [%d-%d][%d-%d] traitée\n", x, x + width - 1, y,
-               y + height - 1);
 
-  monitoring_start_tile (who);
-
-  for (int i = y; i < y + height; i++)
-    for (int j = x; j < x + width; j++) {
-      compute_new_state (i, j);
-    }
-  #pragma omp barrier
-  for (int i = y; i < y + height; i++)
-    for (int j = x; j < x + width; j++) {
-      compute_new_state (i, j);
-    }
-  
-  monitoring_end_tile (x, y, width, height, who);
-}
-
-unsigned sable_compute_tiled (unsigned nb_iter)
-{
-  for (unsigned it = 1; it <= nb_iter; it++) {
-    changement = 0;
-    #pragma omp parallel for collapse(2)
-    for (int y = 0; y < DIM; y += TILE_SIZE)
-      for (int x = 0; x < DIM; x += TILE_SIZE)
-        do_tile_tiled (x + (x == 0), y + (y == 0),
-                  TILE_SIZE - ((x + TILE_SIZE == DIM) + (x == 0)),
-                  TILE_SIZE - ((y + TILE_SIZE == DIM) + (y == 0)),
-                    omp_get_thread_num());
-    if (changement == 0)
-      return it;
-  }
-
-  return 0;
-}
 
 // static inline void compute_new_state_opt (int y, int x)
 // {
@@ -228,58 +180,109 @@ unsigned sable_compute_tiled (unsigned nb_iter)
 ///////////////////////////// Version OpenMP avec opérations atomiques (omp_atomic)
 
 
-// static inline void compute_new_state_omp (int y, int x)
-// {
-//   if (table (y, x) >= 4) {
+static inline void compute_new_state_omp (int y, int x)
+{
+  if (table (y, x) >= 4) {
 
-//       unsigned long int div4 = table (y, x) / 4;
-//       // #pragma omp atomic
-//       table (y, x - 1) += div4;
-//       // #pragma omp atomic
-//       table (y, x + 1) += div4;
-//       // #pragma omp atomic
-//       table (y - 1, x) += div4;
-//       // #pragma omp atomic
-//       table (y + 1, x) += div4;
-//       table (y, x) %= 4;
-//       changement = 1;
+      unsigned long int div4 = table (y, x) / 4;
+      // #pragma omp atomic
+      table (y, x - 1) += div4;
+      // #pragma omp atomic
+      table (y, x + 1) += div4;
+      // #pragma omp atomic
+      table (y - 1, x) += div4;
+      // #pragma omp atomic
+      table (y + 1, x) += div4;
+      table (y, x) %= 4;
+      changement = 1;
 
-//   }
-// }
+  }
+}
 
-// static void do_tile_omp (int x, int y, int width, int height, int who)
-// {
-//   PRINT_DEBUG ('c', "tuile [%d-%d][%d-%d] traitée\n", x, x + width - 1, y,
-//                y + height - 1);
+static void do_tile_omp (int x, int y, int width, int height, int who)
+{
+  PRINT_DEBUG ('c', "tuile [%d-%d][%d-%d] traitée\n", x, x + width - 1, y,
+               y + height - 1);
 
-//   monitoring_start_tile (who);
-//   #pragma omp for schedule(dynamic)
-//   for (int i = y; i < y + height; i++)
-//     for (int j = x; j < x + width; j++) {
-//       if(i)
-//         compute_new_state_omp (i, j);
-//     }
-//   monitoring_end_tile (x, y, width, height, who);
-// }
+  monitoring_start_tile (who);
+  #pragma omp for 
+  for (int i = y; i < y + height; i++)
+    for (int j = x; j < x + width; j++) {
+      if(i)
+        compute_new_state_omp (i, j);
+    }
+  monitoring_end_tile (x, y, width, height, who);
+}
 
-// unsigned sable_compute_omp (unsigned nb_iter) 
-// {
+unsigned sable_compute_omp (unsigned nb_iter) 
+{
   
-//   for (unsigned it = 1; it <= nb_iter; it++) {
-//     changement = 0;
-//     #pragma omp parallel
-//     do_tile_omp (1, 1, DIM - 2, DIM - 2, omp_get_thread_num());
-//     if (changement == 0)
-//       return it;
-//     #pragma omp barrier
+  for (unsigned it = 1; it <= nb_iter; it++) {
+    changement = 0;
+    #pragma omp parallel
+    do_tile_omp (1, 1, DIM - 2, DIM - 2, omp_get_thread_num());
+    if (changement == 0)
+      return it;
+    #pragma omp barrier
   
-//   }
-//   return 0;
-// }
+  }
+  return 0;
+}
 
 
 
 // ///////////////////////////// Version séquentielle tuilée (tiled)
+static inline void compute_new_state_tiled (int y, int x)
+{
+  if (table (y, x) >= 4) {
+    unsigned long int div4 = table (y, x) / 4;
+    table (y, x - 1) += div4;
+    table (y, x + 1) += div4;
+    table (y - 1, x) += div4;
+    table (y + 1, x) += div4;
+    table (y, x) %= 4;
+    changement = 1;
+  }
+}
+
+
+static void do_tile_tiled (int x, int y, int width, int height, int who)
+{
+  PRINT_DEBUG ('c', "tuile [%d-%d][%d-%d] traitée\n", x, x + width - 1, y,
+               y + height - 1);
+
+  monitoring_start_tile (who);
+
+  for (int i = y; i < y + height; i++)
+    for (int j = x; j < x + width; j++) {
+      compute_new_state_tiled (i, j);
+    }
+  #pragma omp barrier
+  for (int i = y; i < y + height; i++)
+    for (int j = x; j < x + width; j++) {
+      compute_new_state_tiled (i, j);
+    }
+  #pragma omp barrier
+  monitoring_end_tile (x, y, width, height, who);
+}
+
+unsigned sable_compute_tiled (unsigned nb_iter)
+{
+  for (unsigned it = 1; it <= nb_iter; it++) {
+    changement = 0;
+    #pragma omp parallel for collapse(2) schedule(dynamic)
+    for (int y = 0; y < DIM; y += TILE_SIZE)
+      for (int x = 0; x < DIM; x += TILE_SIZE)
+        do_tile_tiled (x + (x == 0), y + (y == 0),
+                  TILE_SIZE - ((x + TILE_SIZE == DIM) + (x == 0)),
+                  TILE_SIZE - ((y + TILE_SIZE == DIM) + (y == 0)),
+                    omp_get_thread_num());
+    if (changement == 0)
+      return it;
+  }
+
+  return 0;
+}
 
 // static void compute_new_state_tiled (int x, int y)
 // {
