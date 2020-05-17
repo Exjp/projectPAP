864f89da271b861203e22d482e83c73adb103d98
Pourtier Jacques <jacques.pourtier@etu.u-bordeaux.fr>
2020-05-17 14:39:57 +0200

    practice mpi #2

--- hello.c
+++ hello.c
@@ -6,12 +6,11 @@ int main(int argc, char *argv[]){
     int rank, size, taille;
     char str[4];
 
-    taille = 6;
-    size = 3;
-    int tranche = taille / size;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
+    taille = (size - 1) * 2;
+    int tranche = taille / (size - 1);
 
 
     if (rank == 0)
